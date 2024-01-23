bee1da17715ff611c4e1feea19cf6eb6401c1029
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-23 17:42:13 -0300

    criando o botão de tirar foto

--- src/app/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/app/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -2,6 +2,7 @@
 import { ArrowLeft } from "lucide-react";
 import { FeedbackType, FeedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { Camera } from "lucide-react";
 
 interface FeedBackContentSteps {
   feedbacktype: FeedbackType;
@@ -35,16 +36,18 @@ export function FeedbackContentStep({
       <div className="flex w-full gap-2  py-8">
         <form className="my-4 w-full">
           <textarea
-            className="min-h-[112px] w-full min-w-[304px] resize-none rounded-md border border-zinc-600 bg-transparent text-sm placeholder-zinc-400 focus:border-brand-500 focus:ring-1 focus:ring-brand-500 focus:outline-none"
-            placeholder="Conte com detalhes oque está acontecendo..."
-          >
-
-          </textarea>
-          <footer className="flex gap-2 mt-2">
-            <button 
-            className="p-2  bg-brand-500 rounded-md border-transparent flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors focus:ring-offset-zinc-900 focus:ring-brand-500"
-            type="submit">
-                    Enviar Feedback
+            className="min-h-[112px] w-full min-w-[304px] resize-none rounded-md border border-zinc-600 bg-transparent text-sm placeholder-zinc-400 focus:border-brand-500 focus:outline-none focus:ring-1 focus:ring-brand-500 "
+            placeholder="Conte com detalhes oque está acontecendo..." 
+          ></textarea>
+          <footer className="mt-2 flex gap-2">
+            <button type="button" className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors  focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900">
+              <Camera className="h-6 w-6" />
+            </button>
+            <button
+              className="flex-1  items-center justify-center rounded-md border-transparent bg-brand-500 p-2 text-sm transition-colors hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
+              type="submit"
+            >
+              Enviar Feedback
             </button>
           </footer>
         </form>
