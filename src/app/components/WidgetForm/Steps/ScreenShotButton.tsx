5083019fcf08909e2f7bcb1ec9064b1ddbbf6d9a
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-24 11:11:22 -0300

    criando uma animação de spinner ao clicar para tirar a screnshot

--- src/app/components/WidgetForm/Steps/ScreenShotButton.tsx
+++ src/app/components/WidgetForm/Steps/ScreenShotButton.tsx
@@ -1,21 +1,30 @@
 import html2canvas from "html2canvas";
 import { Camera } from "lucide-react";
+import { useState } from "react";
+import { Spinner } from "../../Spinner";
 
 export function ScreenShotButton() {
+  const [isTakingScreenShot, setisTakingScreenShot] = useState<boolean>(false);
   async function handleTakeScreenshot() {
     const canvas = await html2canvas(document.querySelector("html")!);
-    const base64image=  canvas.toDataURL('image/png');
-
-    console.log(base64image)
+    const base64image = canvas.toDataURL("image/png");
+    setisTakingScreenShot(true);
+    console.log(base64image);
+ 
+    setTimeout(() => {
+        setisTakingScreenShot(false);
+    }, 3000)
+   
+    
   }
-
+  
   return (
     <button
       type="button"
       className="rounded-md border-transparent bg-zinc-800 p-2 transition-colors hover:bg-zinc-700  focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
       onClick={() => handleTakeScreenshot()}
     >
-      <Camera className="h-6 w-6" />
+ {isTakingScreenShot ? <Spinner /> : <Camera className="h-6 w-6" />}
     </button>
   );
 }
