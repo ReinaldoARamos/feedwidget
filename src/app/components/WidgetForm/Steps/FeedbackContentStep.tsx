2a9539e4347991c22ed0385329bcc2d246f2049e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-24 15:36:32 -0300

    criando o preview do screenshot

--- src/app/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/app/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -4,6 +4,7 @@ import { FeedbackType, FeedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { Camera } from "lucide-react";
 import { ScreenShotButton } from "./ScreenShotButton";
+import { useState } from "react";
 
 interface FeedBackContentSteps {
   feedbacktype: FeedbackType;
@@ -14,6 +15,7 @@ export function FeedbackContentStep({
   onFeedbackRestartRequested,
 }: FeedBackContentSteps) {
   const feedbackTypeInfo = FeedbackTypes[feedbacktype];
+  const [screenshot, setScreenshot] = useState<string | null>();
   return (
     <>
       <header className="flex flex-col items-center ">
@@ -38,10 +40,10 @@ export function FeedbackContentStep({
         <form className="my-4 w-full">
           <textarea
             className="min-h-[112px] w-full min-w-[304px] resize-none rounded-md border border-zinc-600 bg-transparent text-sm placeholder-zinc-400 focus:border-brand-500 focus:outline-none focus:ring-1 focus:ring-brand-500 "
-            placeholder="Conte com detalhes oque está acontecendo..." 
+            placeholder="Conte com detalhes oque está acontecendo..."
           ></textarea>
           <footer className="mt-2 flex gap-2">
-            <ScreenShotButton />
+            <ScreenShotButton onScreenShootTook={setScreenshot}  screenshot={screenshot ?? ""}/>
             <button
               className="flex-1  items-center justify-center rounded-md border-transparent bg-brand-500 p-2 text-sm transition-colors hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
               type="submit"
--- src/app/components/WidgetForm/Steps/ScreenShotButton.tsx
+++ src/app/components/WidgetForm/Steps/ScreenShotButton.tsx
@@ -2,29 +2,50 @@ import html2canvas from "html2canvas";
 import { Camera } from "lucide-react";
 import { useState } from "react";
 import { Spinner } from "../../Spinner";
+import { Trash } from "lucide-react";
 
-export function ScreenShotButton() {
+interface ScreenShootButtonPRops {
+  onScreenShootTook: (screenshot: string) => void;
+  screenshot: string;
+}
+export function ScreenShotButton({
+  onScreenShootTook,
+  screenshot,
+}: ScreenShootButtonPRops) {
   const [isTakingScreenShot, setisTakingScreenShot] = useState<boolean>(false);
   async function handleTakeScreenshot() {
     const canvas = await html2canvas(document.querySelector("html")!);
     const base64image = canvas.toDataURL("image/png");
     setisTakingScreenShot(true);
     console.log(base64image);
- 
+    onScreenShootTook(base64image);
     setTimeout(() => {
-        setisTakingScreenShot(false);
-    }, 3000)
-   
-    
+      setisTakingScreenShot(false);
+    }, 3000);
   }
-  
+
+  if (screenshot) {
+    return (
+      <button
+        type="button"
+        className="flex h-10 w-10 items-end justify-end rounded-md border-transparent p-1 text-zinc-400 transition-colors hover:text-zinc-100 "
+        style={{
+          backgroundImage: `url(${screenshot})`,
+          backgroundPosition: 'right bottom',
+          backgroundSize: 180
+        }}
+      >
+        <Trash className="" />
+      </button>
+    );
+  }
   return (
     <button
       type="button"
       className="rounded-md border-transparent bg-zinc-800 p-2 transition-colors hover:bg-zinc-700  focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
       onClick={() => handleTakeScreenshot()}
     >
- {isTakingScreenShot ? <Spinner /> : <Camera className="h-6 w-6" />}
+      {isTakingScreenShot ? <Spinner /> : <Camera className="h-6 w-6" />}
     </button>
   );
 }
