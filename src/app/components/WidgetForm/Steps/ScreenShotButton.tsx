8c96c4320cbface92420c7cc75d75f9ee108eece
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-24 10:57:39 -0300

    criando uma função usando htmo2canvas para tirar um screenshot da tela e converter em base64

--- src/app/components/WidgetForm/Steps/ScreenShotButton.tsx
+++ src/app/components/WidgetForm/Steps/ScreenShotButton.tsx
@@ -1,9 +1,21 @@
+import html2canvas from "html2canvas";
 import { Camera } from "lucide-react";
 
 export function ScreenShotButton() {
-    return (
-        <button type="button" className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors  focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900">
-              <Camera className="h-6 w-6" />
-            </button>
-    )
+  async function handleTakeScreenshot() {
+    const canvas = await html2canvas(document.querySelector("html")!);
+    const base64image=  canvas.toDataURL('image/png');
+
+    console.log(base64image)
+  }
+
+  return (
+    <button
+      type="button"
+      className="rounded-md border-transparent bg-zinc-800 p-2 transition-colors hover:bg-zinc-700  focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
+      onClick={() => handleTakeScreenshot()}
+    >
+      <Camera className="h-6 w-6" />
+    </button>
+  );
 }
