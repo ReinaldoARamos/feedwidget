3f8b7b0b67c75abdebd3d2913e50d7f5a4c8fa84
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-24 17:12:28 -0300

    criando a função que resgata o valor do image e do comment, também coloquei disabled no botao

--- src/app/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/app/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -4,7 +4,7 @@ import { FeedbackType, FeedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { Camera } from "lucide-react";
 import { ScreenShotButton } from "./ScreenShotButton";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 interface FeedBackContentSteps {
   feedbacktype: FeedbackType;
@@ -16,6 +16,12 @@ export function FeedbackContentStep({
 }: FeedBackContentSteps) {
   const feedbackTypeInfo = FeedbackTypes[feedbacktype];
   const [screenshot, setScreenshot] = useState<string | null>();
+  const [comment, setComment] = useState<string | null>('');
+
+  function handleSubmitFeedback(event: FormEvent){
+    event.preventDefault()
+console.log(screenshot, comment)
+  }
   return (
     <>
       <header className="flex flex-col items-center ">
@@ -37,16 +43,18 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
       <div className="flex w-full gap-2  py-8">
-        <form className="my-4 w-full">
+        <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
           <textarea
             className="min-h-[112px] w-full min-w-[304px] resize-none rounded-md border border-zinc-600 bg-transparent text-sm placeholder-zinc-400 focus:border-brand-500 focus:outline-none focus:ring-1 focus:ring-brand-500 "
             placeholder="Conte com detalhes oque está acontecendo..."
+            onChange={event => setComment(event.target.value)}
           ></textarea>
           <footer className="mt-2 flex gap-2">
             <ScreenShotButton onScreenShootTook={setScreenshot}  screenshot={screenshot ?? ""}/>
             <button
-              className="flex-1  items-center justify-center rounded-md border-transparent bg-brand-500 p-2 text-sm transition-colors hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
+              className="flex-1 items-center justify-center rounded-md border-transparent bg-brand-500 p-2 text-sm transition-colors hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-zinc-900 disabled:opacity-50 disabled:hover:brand-500 hover:cursor-not-allowed"
               type="submit"
+              disabled={comment?.length === 0}
             >
               Enviar Feedback
             </button>
