4d2d8f3babd548ea53077e5ba332472ace7f928c
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-25 16:59:34 -0300

    usando a biblioteca nodemailer para enviar emails através do backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,17 @@
 import express from "express";
 import { prisma } from "./prisma";
-
+import nodemailer from "nodemailer";
 const app = express();
 
 app.use(express.json());
 
-const teste = {
-  name: "Reinaldo",
-  email: "Gamersolitavi4l@gmail.com",
-  phone: "4899123133",
-};
-
-app.get("/users", (req, res) => {
-  return res.json(teste);
+var transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "33275e58a7a8f8",
+    pass: "1f46ac0d9b8127",
+  },
 });
 
 app.post("/feedback", async (req, res) => {
@@ -24,9 +23,20 @@ app.post("/feedback", async (req, res) => {
       screenshot,
     },
   });
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Reinaldo Ramos <gamersolitavi4l@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #222">`,
+      `<p>Tipo do feedback:  ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `<p>Screenshot: ${screenshot}</p>`,
+      `</div>`,
+    ].join('/n'),
+  });
   return res.status(201).json(feedback);
 });
-
 
 app.listen(3333, () => {
   console.log("hi HTTP SERVER");
