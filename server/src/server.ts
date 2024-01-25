2222681aa00999faf8bad6dd2b601701cc3d5c09
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-25 16:27:23 -0300

    criando rotas da api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,26 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
+
+app.use(express.json());
+
 const teste = {
-    name: "Reinaldo",
-    email: "Gamersolitavi4l@gmail.com",
-    phone:"4899123133"
-}
-app.get('/users', (req, res) => {
-return res.json(teste)
-}) 
+  name: "Reinaldo",
+  email: "Gamersolitavi4l@gmail.com",
+  phone: "4899123133",
+};
+
+app.get("/users", (req, res) => {
+
+  return res.json(teste);
+});
+
+app.post("/feedback", (req, res) => {
+    console.log(req.body);
+  return res.send("Hi");
+
+});
+
 app.listen(3333, () => {
-    console.log("hi HTTP SERVER")
-})
+  console.log("hi HTTP SERVER");
+});
