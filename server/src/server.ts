f371e3b72cbe872fd4608d9d93a401f718d665c3
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-25 15:48:56 -0300

    criando o schema do prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express'
 
-const teste = 1
-
-async function test() {
-    return 'oka'
+const app = express()
+const teste = {
+    name: "Reinaldo",
+    email: "Gamersolitavi4l@gmail.com",
+    phone:"4899123133"
 }
+app.get('/users', (req, res) => {
+return res.json(teste)
+}) 
+app.listen(3333, () => {
+    console.log("hi HTTP SERVER")
+})
