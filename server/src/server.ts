ee22eff002d0e83839a398e9a2e59475252f3d40
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-25 17:45:58 -0300

    check

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import { routes } from "./route";
 
 const app = express();
 
 app.use(express.json());
 
-
+app.use(routes)
 
 
 app.listen(3333, () => {
   console.log("hi HTTP SERVER");
 });
+
