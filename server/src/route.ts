7de47ab1866841eb2a7a445597d3c897abcce112
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-01-26 15:04:16 -0300

    email enviando e dando update na tabela

--- server/src/route.ts
+++ server/src/route.ts
@@ -22,18 +22,6 @@ routes.post("/feedback", async (req, res) => {
     screenshot,
   });
  
-  await transport.sendMail({
-    from: "Equipe Feedget <oi@feedget.com>",
-    to: "Reinaldo Ramos <gamersolitavi4l@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #222">`,
-      `<p>Tipo do feedback:  ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `<p>Screenshot: ${screenshot}</p>`,
-      `</div>`,
-    ].join("/n"),
-  });
-  */
+ 
   return res.status(201).send();
 });
